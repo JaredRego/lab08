548175deed5f002e137e0aa93c6ed26a44c91836
JaredRego <jmlrego@rogers.com>
2018-11-19 14:47:02 +0000

    third save

--- setbits.sh
+++ setbits.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-i = $1
+i=$1
 
-if (($i & 1))
+if (($i &1))
 then 
   gpio write 0 1;
 fi
 
-if (($i >> 1 & 1))
+if (($i >>1 &1))
 then 
   gpio write 1 1;
 fi
 
-if (($i >> 2 & 1))
+if (($i >>2 &1))
 then 
   gpio write 2 1;
 fi
 
-if (($i >> 3 & 1))
+if (($i >>3 &1))
 then 
   gpio write 3 1;
 fi
